54d713e4987c28c10af46b2e4c711611f40d924b
Logan Crocker <lcroc001@ucr.edu>
2019-05-11 01:07:24 -0800

    test

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $("#header-wrapper").animate({opacity: 1}, 500, function() {
-        $("#bio-wrapper").animate({opacity: 1}, 500, function() {
-            $("#technical-skills-wrapper").animate({opacity: 1}, 500, function() {
-                $("#spotify-player-wrapper").animate({opacity: 1}, 500);
-            });
-        });
-    });
-});
